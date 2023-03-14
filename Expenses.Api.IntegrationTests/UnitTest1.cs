55e091b4fd56bc3abe5fdcf59beeb6e68ad96628
saidurraahmaan <saidurraahmaan@gmail.com>
2023-03-15 00:08:52 +0600

    necessary project created

---
+++ Expenses.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Expenses.Api.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Expenses.Queries.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Expenses.Queries.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
