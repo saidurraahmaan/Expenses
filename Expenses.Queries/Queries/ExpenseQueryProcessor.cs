2101234388afc5753a39e73df1b60d9c0d7dbbe5
saidurraahmaan <saidurraahmaan@gmail.com>
2023-03-19 00:17:32 +0600

    container setup

--- Expenses.Queries/Queries/ExpenseQueryProcessor.cs
+++ Expenses.Queries/Queries/ExpenseQueryProcessor.cs
@@ -20,29 +20,75 @@ namespace Expenses.Queries.Queries
             _unitofWork = unitofWork;
             _securityContext = securityContext;
         }
-        public Task<Expense> Create(CreateExpenseModel model)
+        public async Task<Expense> Create(CreateExpenseModel model)
         {
-            throw new NotImplementedException();
+            var item = new Expense
+            {
+                UserId = _securityContext.User.Id,
+                Amount = model.Amount,
+                Comment = model.Comment,
+                Date = model.Date,
+                Description = model.Description,
+            };
+            _unitofWork.Add(model);
+            await _unitofWork.CommitAsync();
+            return item;
+
         }
 
-        public Task Delete(int id)
+        public async Task Delete(int id)
         {
-            throw new NotImplementedException();
+            var user = GetQuery().FirstOrDefault(x => x.Id == id);
+            if(user== null)
+            {
+                throw new KeyNotFoundException("Expense is not found");
+            }
+            if (user.IsDeleted) return;
+            user.IsDeleted = true;
+            await _unitofWork.CommitAsync();
         }
 
         public IQueryable<Expense> Get()
         {
-            throw new NotImplementedException();
+            var query = GetQuery();
+            return query;
+        }
+
+        public IQueryable<Expense> GetQuery()
+        {
+            var q = _unitofWork.Query<Expense>()
+                .Where(x => !x.IsDeleted);
+
+            if (!_securityContext.IsAdministrator)
+            {
+                var userId = _securityContext.User.Id;
+                q = q.Where(x => x.UserId == userId);    
+            }
+            return q;
         }
 
         public Expense GetById(int id)
         {
-            throw new NotImplementedException();
+            var user = GetQuery().FirstOrDefault(x=>x.Id==id);
+            if (user == null)
+            {
+                throw new KeyNotFoundException("Expense is not found");
+            }
+            return user;
         }
 
-        public Task<Expense> Update(int id, UpdateExpenseModel model)
+        public async Task<Expense> Update(int id, UpdateExpenseModel model)
         {
-            throw new NotImplementedException();
+            var expense = GetQuery().FirstOrDefault(x => x.Id == id);
+            if(expense == null) {
+                throw new KeyNotFoundException("Expense is not found");
+            }
+            expense.Amount = model.Amount;
+            expense.Comment = model.Comment;
+            expense.Description = model.Description;
+            expense.Date = model.Date;
+            await _unitofWork.CommitAsync();
+            return expense;
         }
     }
 }
---
+++ Expenses/IoC/ContainerSetup.cs
@@ -0,0 +1,26 @@
+﻿using Expenses.Data.Access.DAL;
+using Expenses.Data.Access.DAL.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expenses.IoC
+{
+    public static class ContainerSetup
+    {
+        public static void Setup(IServiceCollection services,IConfigurationRoot configuration)
+        {
+            //AddUow()
+        }
+
+        private static void AddUow(IServiceCollection services,IConfigurationRoot configuration)
+        {
+            var connectionString = configuration.GetConnectionString("main");
+            services.AddEntityFrameworkSqlServer();
+            services.AddDbContext<MainDbContext>(option =>
+            {
+                option.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IUnitofWork>(x => new EFUnitOfWork(x.GetRequiredService<MainDbContext>()));
+
+        }
+    }
+}
